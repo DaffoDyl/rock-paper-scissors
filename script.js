c286cfc5fa8fa5a0c16b5872b6ab90e5d8cac422
Dylan Armstrong <dylan.armstrong299@gmail.com>
2022-06-02 03:55:03 -0600

    Get game running

--- script.js
+++ script.js
@@ -11,4 +11,33 @@ let computerPlay = () => {
     }
 }
 
+let play = (player, computer) => {
+    let results;
+    switch(player) {
+        case "rock":
+            (computer == "rock") ? results = "tie" : (computer == "paper") ? results = "loss" : results = "win";
+            break;
+        case "paper":
+            (computer == "paper") ? results = "tie" : (computer == "scissors") ? results = "loss" : results = "win";
+            break;
+        case "scissors":
+            (computer == "scissors") ? results = "tie" : (computer == "rock") ? results = "loss" : results = "win";
+    }
+    
+    player = player[0].toUpperCase() + player.slice(1);
+    switch(results) {
+        case "tie":
+            return `You tied! ${player} ties ${computer}.`;
+        case "loss":
+            return `You lost! ${player} losses to ${computer}.`;
+        case "win":
+            return `You won! ${player} beats ${computer}.`;
+        default:
+            return `Invalid input: ${player} not recognized`;
+    }
+}
+
 let computerSelection = computerPlay();
+let playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
+
+console.log(play(playerSelection, computerSelection));
