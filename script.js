23602847bd0c22027b7014d088ae6044be905ab4
Dylan Armstrong <dylan.armstrong299@gmail.com>
2022-06-02 03:23:45 -0600

    Set up computers selection

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World!")
+let computerPlay = () => {
+    let selection = Math.floor(Math.random() * 3);
+
+    switch(selection) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
+
+let computerSelection = computerPlay();
